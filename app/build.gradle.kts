abddac3b40223e1fe65b338854237e824da3b9a9
Krishan kumar sharma <krishan@alphacoach.app>
2024-04-11 09:53:02 +0530

    versionCode = 22 versionName = "4.9"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.gb.restaurant"
         minSdk = 24
         targetSdk = 34
-        versionCode = 21
-        versionName = "4.8"
+        versionCode = 22
+        versionName = "4.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
